688267d93549d08cb6a8c65405f60c6f85000884
magralo <magralo@gmail.com>
2019-12-18 10:25:31 -0500

    check dependencies before app

--- app.R
+++ app.R
@@ -1,3 +1,35 @@
+dep=c("rsconnect" ,"shiny","mlogit" ,"MASS" ,"AER" ,
+      "mcmc","bayesm",
+      "abind","bayesboot","bibtex" ,
+      "BMA","car","carData","cellranger" ,"clipr",
+      "coda","DT","Formula","ivbma",
+      "Matrix" ,"matrixcalc","RcppEigen",
+      "Rdpack","rhandsontable" ,"statmod", "truncnorm"  ,
+      "stringr","MCMCpack")
+
+needed=dep[!dep%in%installed.packages()]
+
+if (length(needed)>0){
+  for (p in needed){
+    install.packages(p)
+  }
+}
+
+needed=dep[!dep%in%installed.packages()]
+
+
+if (length(needed)>0){
+  for (p in needed){
+    if (p=="ivbma"){
+      install.packages("https://cran.r-project.org/src/contrib/Archive/ivbma/ivbma_1.05.tar.gz", repos = NULL, type = "source")
+    }else if (p=="MCMCpack"){
+      install.packages("https://cran.r-project.org/src/contrib/Archive/MCMCpack/MCMCpack_1.4-4.tar.gz", repos = NULL, type = "source")
+    }else{
+      install.packages(p)
+    }
+  }
+}
+
 
 library(shiny)
 options(shiny.maxRequestSize=100*1024^2)
